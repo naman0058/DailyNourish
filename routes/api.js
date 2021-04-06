c883c67deb176c6d0e02c477c2cca88e985ed8f6
SpvaigPvtLtd <info@spvaig.in>
2021-04-06 19:20:07 +0530

    a

--- routes/api.js
+++ routes/api.js
@@ -501,6 +501,7 @@ router.get('/time',(req,res)=>{
 
 router.post("/cart-handler", (req, res) => {
         let body = req.body
+        console.log(req.body)
         if (req.body.quantity == "0" || req.body.quantity == 0) {
         pool.query(`delete from cart where booking_id = '${req.body.booking_id}' and  usernumber = '${req.body.usernumber}'`,(err,result)=>{
             if (err) throw err;
@@ -527,7 +528,7 @@ router.post("/cart-handler", (req, res) => {
                     })
                 }
                 else {
-                  body["price"] = (+req.body.net_amount)*(+req.body.quantity);
+                  body["price"] = 50
                      pool.query(`insert into cart set ?`, body, (err, result) => {
                      if (err) throw err;
                      else {
